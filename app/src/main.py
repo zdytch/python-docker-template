e9d42d68123c380c777b6a17d124770882d95a67
Alexey Luzin <zdytch@gmail.com>
2023-02-23 14:50:23 +0200

    Add logger

--- app/src/main.py
+++ app/src/main.py
@@ -1,4 +1,14 @@
+from loguru import logger
 from settings import DEBUG
+
+logger.add(
+    'logs/{time}.log',
+    format='{time} {level} {message}',
+    level='DEBUG',
+    rotation='100 MB',
+    retention='14 days',
+    compression='zip',
+)
 
 if DEBUG:
     import debugpy
